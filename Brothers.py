8c8685f38e2ac45cae7ef975fba2e579076ec61c
hamziusmananwar <67110401+hamziusmananwar@users.noreply.github.com>
2020-06-18 22:08:39 +0500

    Update and rename zeroboy.py to Brothers.py

--- Brothers.py
+++ Brothers.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - BROTHERS -OFFICAL------•◈•
+|__\~~) •||•HANAN-HAMZI-USMAN---------------•◈•
+|__(-----\  •◈•------BROTHERXX---------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBROTHERS
+\033[1;96mWhatsapp \033[1;93m: \033[1;+6285202695413
+\033[1;96mFacebook  \033[1;93m: \033[1; HATERXX.KA.PAPA.HANAN.001
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/hamziusmananwar
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBROTHERS
+\033[1;96mWhatsapp \033[1;93m: \033[1;92m+6285202695413
+\033[1;96mFacebook  \033[1;93m: \033[1;92m HATERXX.KA.PAPA.HANAN.001
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/hamziusmananwar
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "brothers"
+CorrectPassword = "Brothers"
 
 loop = 'true'
 while (loop == 'true'):
